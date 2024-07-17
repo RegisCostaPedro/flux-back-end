a70b3401f28d3e958e98a60cc4253d82b1828d31
regis <regiscostapedro0705@gmail.com>
2024-07-16 23:47:59 -0300

    Feat: Usuario entity criado

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -1,11 +1,10 @@
-const conexao = require('../config');
+const conexao = require('../../bin/database');
 
 const { Sequelize, DataTypes } = require('sequelize');
 
 const Usuario = conexao.define('usuario', {
   id_usuario: {
     type: DataTypes.INTEGER,
-
     allowNull: false,
     primaryKey: true,
     autoIncrement: true
@@ -17,11 +16,13 @@ const Usuario = conexao.define('usuario', {
   },
   cpf: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    unique: true
   },
   email: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    unique: true
   },
   senha: {
     type: DataTypes.STRING,
@@ -34,7 +35,7 @@ const Usuario = conexao.define('usuario', {
 
 
 
-Usuario.sync({ force: true })
-  .then(() => console.log('Tabela "usuario" criada com sucesso!'))
-  .catch(err => console.error('Erro ao criar a tabela "usuario":', err));
+//  Usuario.sync({ force: true })
+//    .then(() => console.log('Tabela "usuario" criada com sucesso!'))
+//    .catch(err => console.error('Erro ao criar a tabela "usuario":', err));
 module.exports = Usuario;
