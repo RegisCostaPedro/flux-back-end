80a7d7e98ab4bf1b5539e954b587b9f848a46424
regis <regiscostapedro0705@gmail.com>
2024-08-06 22:59:54 -0300

    Fix: Ajustando query de transações recentes

--- src/repositories/home-repository.js
+++ src/repositories/home-repository.js
@@ -1,37 +1,41 @@
 
 const Banco = require("../models/banco");
 const Conta = require('../models/conta');
-const {Sequelize, QueryTypes, where} = require('sequelize');
+const { Sequelize, QueryTypes, where } = require('sequelize');
 
 class HomeRepository {
 
-    static get = async (id_user,usuario_nome_token) => {
+    static get = async (id_user, usuario_nome_token) => {
 
-      const query = await Banco.sequelize
+        const query = await Banco.sequelize
             .query(`
-        SELECT usuario.nome,conta.saldo, banco.nome_banco, banco.descricao 
-        FROM conta 
-        JOIN banco ON banco.id_banco = conta.banco_id 
-        JOIN usuario ON usuario.id_usuario = conta.usuario_id 
+         SELECT usuario.nome,
+ transacao.valor,
+ transacao.descricao
+ FROM transacao
+        JOIN banco on banco.id_banco = transacao.banco_id JOIN
+        conta ON conta.id_conta = transacao.conta_id
+        JOIN usuario ON usuario.id_usuario = transacao.usuario_id
         WHERE usuario.id_usuario = :id_user   
-        `,{
-            replacements: {id_user :id_user},
-            type: QueryTypes.SELECT
-        });
+        `, {
+                replacements: { id_user: id_user },
+                type: QueryTypes.SELECT
+            });
 
 
         // Cso o usuário não tenha nenhuma notificação, retornara apenas o nome dele
-        if(!query || query.length === 0){
-            return {status: 200 , data:  {
-                nome: usuario_nome_token ,
-                saldo: parseFloat(0.00),
-                nome_banco: null,
-                descricao: null,
-            }
-        };
+        if (!query || query.length === 0) {
+            return {
+                status: 200, data: {
+                    nome: usuario_nome_token,
+                    saldo: parseFloat(0.00),
+                    nome_banco: null,
+                    descricao: null,
+                }
+            };
         }
-      
-        return {status: 200, data:  query};
+
+        return { status: 200, data: query };
     }
 
 
