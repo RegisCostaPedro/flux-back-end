83cc9b66933282a9e17e326abc10a89c7a2265b7
regis <regiscostapedro0705@gmail.com>
2024-11-29 00:33:13 -0300

    Fix: Ajustando requisição de criação de chaves Pix

--- src/controllers/pix-controller.js
+++ src/controllers/pix-controller.js
@@ -1,195 +1,197 @@
-const axios = require('axios');
-const repository = require('../repositories/pix-repository');
-const authServiceAPI = require('../services/auth-transfeera-service');
-const authService = require('../services/auth-service');
-const pixService = require('../services/pix-service');
+const axios = require("axios");
+const repository = require("../repositories/pix-repository");
+const authServiceAPI = require("../services/auth-transfeera-service");
+const authService = require("../services/auth-service");
+const pixService = require("../services/pix-service");
 
-
-require('dotenv').config();
+require("dotenv").config();
 
 class PixController {
-    static criarChave = async (req, res) => {
-        try {
-            const token = req.body.token || req.query.token || req.headers['x-access-token'];
-            const dadosUsuario = await authService.decodeToken(token);
-            const banco_id = req.body.banco_id
-            const contaBancaria_id = req.body.conta_bancaria_id
-            const { key_type, key } = req.body;
+  static criarChave = async (req, res) => {
+    try {
+      const token =
+        req.body.token || req.query.token || req.headers["x-access-token"];
+      const dadosUsuario = await authService.decodeToken(token);
+      const banco_id = req.body.banco_id;
+      const contaBancaria_id = req.body.conta_bancaria_id;
+      const { key_type, key } = req.body;
 
+      const accessToken = await authServiceAPI.returnAccessToken();
 
-            const accessToken = await authServiceAPI.returnAccessToken();
+      const response = await pixService.criarChave(
+        key_type,
+        key,
+        dadosUsuario,
+        accessToken,
+        banco_id,
+        contaBancaria_id
+      );
 
-            const response = await pixService.criarChave(
-                key_type,
-                key,
-                dadosUsuario,
-                accessToken,
-                banco_id,
-                contaBancaria_id);
-
-
-                if (response.status === 200) {
-                    return res.status(200).send(response.data);
-                }
-                 else {
-                    return res.status(response.status).send({ message: response.message });
-                }
-
-        } catch (error) {
-            res.status(400).send({
-                message: "Falha ao processar requisição: " + error
-            })
-
-        }
+      if (response.status === 201) {
+        return res.status(response.status).json(response.data);
+      } else {
+        return res.status(response.status).send({ message: response.message });
+      }
+    } catch (error) {
+      res.status(400).send({
+        message: "Falha ao processar requisição: " + error,
+      });
     }
+  };
 
-    static verificarChave = async (req, res) => {
-        try {
-            const idPix = req.params.id;
-            const verifyCode = req.body.code;
-         
-            console.log('verifyCode: ', verifyCode);
-            const token = req.body.token || req.query.token || req.headers['x-access-token'];
-            const dadosUsuario = await authService.decodeToken(token);
-            const emailUsuario = dadosUsuario.email;
+  static verificarChave = async (req, res) => {
+    try {
+      const idPix = req.params.id;
+      const verifyCode = req.body.code;
 
-            const accessToken = await authServiceAPI.returnAccessToken();
-            const response = await pixService.verificarChave(idPix, emailUsuario, accessToken, verifyCode);
-            console.log('verifyCode: ', verifyCode);
-            console.log(response.data);
+      console.log("verifyCode: ", verifyCode);
+      const token =
+        req.body.token || req.query.token || req.headers["x-access-token"];
+      const dadosUsuario = await authService.decodeToken(token);
+      const emailUsuario = dadosUsuario.email;
 
-            if (response.status === 200) {
-                return res.status(200).send(response.data);
-            }
-             else {
-                return res.status(response.status).send({ message: response.message });
-            }
+      const accessToken = await authServiceAPI.returnAccessToken();
+      const response = await pixService.verificarChave(
+        idPix,
+        emailUsuario,
+        accessToken,
+        verifyCode
+      );
+      console.log("verifyCode: ", verifyCode);
+      console.log(response.data);
 
-        } catch (error) {
-
-            res.status(400).send({
-                message: "Falha ao processar requisição: " + error
-            })
-        }
-
+      if (response.status === 200) {
+        return res.status(200).send(response.data);
+      } else {
+        return res.status(response.status).send({ message: response.message });
+      }
+    } catch (error) {
+      res.status(400).send({
+        message: "Falha ao processar requisição: " + error,
+      });
     }
+  };
 
-    static reenviarCodigo = async (req, res) => {
-        try {
+  static reenviarCodigo = async (req, res) => {
+    try {
+      const idPix = req.params.id;
 
-            const idPix = req.params.id;
+      const token =
+        req.body.token || req.query.token || req.headers["x-access-token"];
+      const dadosUsuario = await authService.decodeToken(token);
+      const emailUsuario = dadosUsuario.email;
 
+      const accessToken = await authServiceAPI.returnAccessToken();
+      const response = await pixService.reenviarCodigo(
+        idPix,
+        emailUsuario,
+        accessToken
+      );
 
-            const token = req.body.token || req.query.token || req.headers['x-access-token'];
-            const dadosUsuario = await authService.decodeToken(token);
-            const emailUsuario = dadosUsuario.email;
+      console.log(response.data);
 
-            const accessToken = await authServiceAPI.returnAccessToken();
-            const response = await pixService.reenviarCodigo(idPix, emailUsuario, accessToken);
-
-            console.log(response.data);
-
-            if (response.status === 200) {
-                return res.status(200).send(response.data);
-            } else {
-                return res.status(response.status).send({ message: response.message });
-            }
-
-        } catch (error) {
-
-            res.status(400).send({
-                message: "Falha ao processar requisição: " + error
-            })
-        }
-
+      if (response.status === 200) {
+        return res.status(200).send(response.data);
+      } else {
+        return res.status(response.status).send({ message: response.message });
+      }
+    } catch (error) {
+      res.status(400).send({
+        message: "Falha ao processar requisição: " + error,
+      });
     }
+  };
 
-    static listarChavesPix = async (req, res) => {
-        try {
-            //Recupera o token
-            const token = req.body.token || req.query.token || req.headers['x-access-token'];
+  static listarChavesPix = async (req, res) => {
+    try {
+      //Recupera o token
+      const token =
+        req.body.token || req.query.token || req.headers["x-access-token"];
 
-            // Decodifica o token
-            const dadosUsuario = await authService.decodeToken(token);
+      // Decodifica o token
+      const dadosUsuario = await authService.decodeToken(token);
 
-            const pix = await repository.get(dadosUsuario.id);
+      const pix = await repository.get(dadosUsuario.id);
 
-            if (pix.status === 200) {
-                return res.status(pix.status).send(pix.data);
-            } else {
-
-                return res.status(pix.status).send({ message: pix.message });
-            }
-
-        } catch (error) {
-            res.status(400).send({
-                message: "Falha ao processar requisição: " + error
-            })
-        }
-
+      if (pix.status === 200) {
+        return res.status(pix.status).send(pix.data);
+      } else {
+        return res.status(pix.status).send({ message: pix.message });
+      }
+    } catch (error) {
+      res.status(400).send({
+        message: "Falha ao processar requisição: " + error,
+      });
     }
+  };
 
-    static buscarChavePixPorID = async (req, res) => {
-        try {
-            const idPix = req.params.id;
+  static buscarChavePixPorID = async (req, res) => {
+    try {
+      const idPix = req.params.id;
 
-            const token = req.body.token || req.query.token || req.headers['x-access-token'];
-            const dadosUsuario = await authService.decodeToken(token);
+      const token =
+        req.body.token || req.query.token || req.headers["x-access-token"];
+      const dadosUsuario = await authService.decodeToken(token);
 
-            const accessToken = await authServiceAPI.returnAccessToken();
+      const accessToken = await authServiceAPI.returnAccessToken();
 
-            const response = await pixService.buscarChaveId(idPix, dadosUsuario, accessToken);
+      const response = await pixService.buscarChaveId(
+        idPix,
+        dadosUsuario,
+        accessToken
+      );
 
-
-            if (response.status !== 200) {
-                return res.status(response.status).send(response.message);
-            }
-            return res.status(response.status).send(response.data);
-
-        } catch (error) {
-            res.status(400).send({
-                message: "Falha ao processar requisição: " + error
-            })
-        }
+      if (response.status !== 200) {
+        return res.status(response.status).send(response.message);
+      }
+      return res.status(response.status).send(response.data);
+    } catch (error) {
+      res.status(400).send({
+        message: "Falha ao processar requisição: " + error,
+      });
     }
+  };
 
-    static deletarChave = async (req, res) => {
-        try {
-            const idPix = req.params.id;
-            const token = req.body.token || req.query.token || req.headers['x-access-token'];
+  static deletarChave = async (req, res) => {
+    try {
+      const idPix = req.params.id;
+      const token =
+        req.body.token || req.query.token || req.headers["x-access-token"];
 
-            // Decodifica o token
-            const dadosUsuario = await authService.decodeToken(token);
-            const usuario_id = dadosUsuario.id;
-            const emailUsuario = dadosUsuario.email;
-            const accessToken = await authServiceAPI.returnAccessToken();
+      // Decodifica o token
+      const dadosUsuario = await authService.decodeToken(token);
+      const usuario_id = dadosUsuario.id;
+      const emailUsuario = dadosUsuario.email;
+      const accessToken = await authServiceAPI.returnAccessToken();
 
-            const response = await pixService.deletarChave(idPix, usuario_id, emailUsuario, accessToken);
+      const response = await pixService.deletarChave(
+        idPix,
+        usuario_id,
+        emailUsuario,
+        accessToken
+      );
 
-            
-            console.log(response.status);
+      console.log(response.status);
 
-            if (response.status === 200) {
-                console.warn(response.status);
-                console.warn(response.status);
-                console.warn(response.status);
-                return res.status(response.status).json(response.data);
-            }else if(response.status === 201){
-                console.warn(response.status);
-                console.warn(response.status);
-                console.warn(response.status);
-                return res.status(response.status).json(response.data);
-            } else {
-                return res.status(response.status).send({ message: response.message });
-            }
-
-        } catch (error) {
-
-            res.status(400).send({
-                message: "Falha ao processar requisição: " + error
-            })
-        }
+      if (response.status === 200) {
+        console.warn(response.status);
+        console.warn(response.status);
+        console.warn(response.status);
+        return res.status(response.status).json(response.data);
+      } else if (response.status === 201) {
+        console.warn(response.status);
+        console.warn(response.status);
+        console.warn(response.status);
+        return res.status(response.status).json(response.data);
+      } else {
+        return res.status(response.status).send({ message: response.message });
+      }
+    } catch (error) {
+      res.status(400).send({
+        message: "Falha ao processar requisição: " + error,
+      });
     }
+  };
 }
 
 module.exports = PixController;
